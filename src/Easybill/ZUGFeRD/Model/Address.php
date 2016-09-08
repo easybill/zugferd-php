e5d74502a448f0adf2214adb789464828303dc6b
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-08 10:14:20 +0200

    Add first version Address-class.

---
+++ src/Easybill/ZUGFeRD/Model/Address.php
@@ -0,0 +1,136 @@
+<?php
+
+
+namespace Easybill\ZUGFeRD\Model;
+use JMS\Serializer\Annotation\XmlNamespace;
+use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\SerializedName;
+
+/**
+ * Class Address
+ *
+ * @XmlNamespace(uri="urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12", prefix="ram")
+ *
+ * @package Easybill\ZUGFeRD\Model
+ */
+class Address
+{
+
+    /**
+     * @var string
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("PostcodeCode")
+     */
+    private $postcode = '';
+
+    /**
+     * @var string
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("LineOne")
+     */
+    private $lineOne = '';
+
+    /**
+     * @var string
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("LineTwo")
+     */
+    private $lineTwo = '';
+
+    /**
+     * @var string
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("CityName")
+     */
+    private $city = '';
+
+    /**
+     * @var string
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("CountryID")
+     */
+    private $countryCode = '';
+
+    /**
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * @param string $postcode
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLineOne()
+    {
+        return $this->lineOne;
+    }
+
+    /**
+     * @param string $lineOne
+     */
+    public function setLineOne($lineOne)
+    {
+        $this->lineOne = $lineOne;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLineTwo()
+    {
+        return $this->lineTwo;
+    }
+
+    /**
+     * @param string $lineTwo
+     */
+    public function setLineTwo($lineTwo)
+    {
+        $this->lineTwo = $lineTwo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     */
+    public function setCountryCode($countryCode)
+    {
+        $this->countryCode = $countryCode;
+    }
+
+
+
+}
