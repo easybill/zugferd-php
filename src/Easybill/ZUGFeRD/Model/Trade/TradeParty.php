593ff7228e2d9e6dfd26d667052101d5e681f1bd
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-08 13:59:26 +0200

    Use the address an tax registration on TradeParty.

--- src/Easybill/ZUGFeRD/Model/Trade/TradeParty.php
+++ src/Easybill/ZUGFeRD/Model/Trade/TradeParty.php
@@ -3,9 +3,14 @@
 
 namespace Easybill\ZUGFeRD\Model\Trade;
 
+use Easybill\ZUGFeRD\Model\Address;
+use Easybill\ZUGFeRD\Model\Trade\Tax\Registration;
+use Easybill\ZUGFeRD\Model\Trade\Tax\TaxRegistration;
 use JMS\Serializer\Annotation\XmlNamespace;
 use JMS\Serializer\Annotation\XmlElement;
 use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlList;
 
 /**
  * Class TradeParty
@@ -17,9 +22,11 @@ use JMS\Serializer\Annotation\SerializedName;
 class TradeParty
 {
 
-    public function __construct($name = '')
+    public function __construct($name = '', Address $address, array $taxRegistrations)
     {
         $this->name = $name;
+        $this->address = $address;
+        $this->taxRegistrations = $taxRegistrations;
     }
 
     /**
@@ -28,6 +35,21 @@ class TradeParty
      * @SerializedName("Name")
      */
     private $name = '';
+
+    /**
+     * @var Address
+     * @Type("Easybill\ZUGFeRD\Model\Address")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("PostalTradeAddress")
+     */
+    private $address;
+
+    /**
+     * @var TaxRegistration[]
+     * @Type("array<Easybill\ZUGFeRD\Model\Trade\Tax\TaxRegistration>")
+     * @XmlList(inline = true, entry = "SpecifiedTaxRegistration", namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     */
+    private $taxRegistrations = array();
 
     /**
      * @return string
@@ -47,5 +69,46 @@ class TradeParty
         $this->name = $name;
         return $this;
     }
+
+    /**
+     * @return \Easybill\ZUGFeRD\Model\Address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param \Easybill\ZUGFeRD\Model\Address $address
+     *
+     * @return self
+     */
+    public function setAddress(Address $address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return \Easybill\ZUGFeRD\Model\Trade\Tax\TaxRegistration[]
+     */
+    public function getTaxRegistration()
+    {
+        return $this->taxRegistrations;
+    }
+
+    /**
+     * @param \Easybill\ZUGFeRD\Model\Trade\Tax\TaxRegistration $taxRegistration
+     *
+     * @return self
+     */
+    public function addTaxRegistration(TaxRegistration $taxRegistration)
+    {
+        $this->taxRegistrations[] = $taxRegistration;
+        return $this;
+    }
+
+
+
 
 }
