f7d01f31415731e8b0aaf73b93f62685e29cc0b5
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-09 14:03:37 +0200

    Add CreditorFinancialAccount and -Institution classes.

---
+++ src/Easybill/ZUGFeRD/Model/CreditorFinancialAccount.php
@@ -0,0 +1,101 @@
+<?php
+
+
+namespace Easybill\ZUGFeRD\Model;
+
+use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+
+class CreditorFinancialAccount
+{
+
+    /**
+     * IBAN (International Bank Account Number) of the bank account.
+     *
+     * @var string
+     * @Type("string")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("IBANID")
+     */
+    private $iban = '';
+
+    /**
+     * @var string
+     * @Type("string")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("AccountName")
+     */
+    private $accountName = '';
+
+    /**
+     * @var string
+     * @Type("string")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("ProprietaryID")
+     */
+    private $proprietary = '';
+
+    /**
+     * CreditorFinancialAccount constructor.
+     *
+     * @param string $iban
+     * @param string $accountName
+     * @param string $proprietary
+     */
+    public function __construct($iban, $accountName, $proprietary)
+    {
+        $this->iban = $iban;
+        $this->accountName = $accountName;
+        $this->proprietary = $proprietary;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIban()
+    {
+        return $this->iban;
+    }
+
+    /**
+     * @param string $iban
+     */
+    public function setIban($iban)
+    {
+        $this->iban = $iban;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountName()
+    {
+        return $this->accountName;
+    }
+
+    /**
+     * @param string $accountName
+     */
+    public function setAccountName($accountName)
+    {
+        $this->accountName = $accountName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProprietary()
+    {
+        return $this->proprietary;
+    }
+
+    /**
+     * @param string $proprietary
+     */
+    public function setProprietary($proprietary)
+    {
+        $this->proprietary = $proprietary;
+    }
+
+}
