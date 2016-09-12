f67ec6d6116cd57ae1506b751967762dce57f14a
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-12 07:50:07 +0200

    Add TradeTax class.

---
+++ src/Easybill/ZUGFeRD/Model/Trade/Tax/TradeTax.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace Easybill\ZUGFeRD\Model\Trade\Tax;
+
+use Easybill\ZUGFeRD\Model\Trade\Amount;
+use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Class TradeTax
+ *
+ * @package Easybill\ZUGFeRD\Model\Trade
+ */
+class TradeTax
+{
+    /**
+     * @var Amount
+     * @Type("Easybill\ZUGFeRD\Model\Trade\Amount")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("CalculatedAmount")
+     */
+    private $calculatedAmount;
+
+    /**
+     * @var string
+     * @Type("string")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("TypeCode")
+     */
+    private $code = '';
+
+    /**
+     * @var Amount
+     * @Type("Easybill\ZUGFeRD\Model\Trade\Amount")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("BasisAmount")
+     */
+    private $basisAmount;
+
+    /**
+     * @var double
+     * @Type("double")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("ApplicablePercent")
+     */
+    private $percent = 0.0;
+
+    /**
+     * @return \Easybill\ZUGFeRD\Model\Trade\Amount
+     */
+    public function getCalculatedAmount()
+    {
+        return $this->calculatedAmount;
+    }
+
+    /**
+     * @param \Easybill\ZUGFeRD\Model\Trade\Amount $calculatedAmount
+     */
+    public function setCalculatedAmount($calculatedAmount)
+    {
+        $this->calculatedAmount = $calculatedAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return \Easybill\ZUGFeRD\Model\Trade\Amount
+     */
+    public function getBasisAmount()
+    {
+        return $this->basisAmount;
+    }
+
+    /**
+     * @param \Easybill\ZUGFeRD\Model\Trade\Amount $basisAmount
+     */
+    public function setBasisAmount($basisAmount)
+    {
+        $this->basisAmount = $basisAmount;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPercent()
+    {
+        return $this->percent;
+    }
+
+    /**
+     * @param float $percent
+     */
+    public function setPercent($percent)
+    {
+        $this->percent = number_format($percent, 2);
+    }
+
+}
