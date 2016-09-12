b38f56da6034616d264099a5cade2a9c9fa1e4e2
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-12 15:08:10 +0200

    Add first version of SpecifiedTradeDelivery class.

---
+++ src/Easybill/ZUGFeRD/Model/Trade/item/SpecifiedTradeDelivery.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Easybill\ZUGFeRD\Model\Trade\Item;
+
+use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Class SpecifiedTradeDelivery
+ *
+ * @package Easybill\ZUGFeRD\Model\Trade\Item
+ */
+class SpecifiedTradeDelivery
+{
+
+    /**
+     * @var Quantity
+     * @Type("Easybill\ZUGFeRD\Model\Trade\Item\Quantity")
+     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("BilledQuantity")
+     */
+    private $billedQuantity;
+
+    /**
+     * SpecifiedTradeDelivery constructor.
+     *
+     * @param Quantity $billedQuantity
+     */
+    public function __construct(Quantity $billedQuantity)
+    {
+        $this->billedQuantity = $billedQuantity;
+    }
+
+}
