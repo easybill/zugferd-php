0675fda7636db8dc3dce6bf5f23a58ffd1f5767f
Sascha Hofrichter <hofrichter@easybill.de>
2016-09-14 08:50:59 +0200

    Remove unused setter functions and add some validation into the constructor.

--- src/Easybill/ZUGFeRD/Model/DateTime.php
+++ src/Easybill/ZUGFeRD/Model/DateTime.php
@@ -15,12 +15,40 @@ class DateTime
     /**
      * DateTime constructor.
      *
-     * @param string $time
-     * @param int    $format
+     * @param \DateTime|string $time
+     * @param int              $format
      */
     public function __construct($time, $format = 102)
     {
-        $this->time = $time;
+
+        if ($format !== 102 && $format !== 610 && $format !== 616) {
+            throw new \RuntimeException('Invalid format! Please set it to: 102, 610 or 616');
+        }
+
+        if ($time instanceof \DateTime) {
+            $dateTime = $time;
+        } else if (is_string($time)) {
+            $dateTime = new \DateTime($time);
+        } else {
+            throw new \RuntimeException('Invalid date! it must be an instance of \DateTime or must be a string!');
+        }
+
+        switch ($format) {
+            case 616:
+                $formatStr = 'YW';
+                break;
+
+            case 610:
+                $formatStr = 'Ym';
+                break;
+
+            case 102:
+            default:
+                $formatStr = 'Ymd';
+
+        }
+
+        $this->time = $dateTime->format($formatStr);
         $this->format = $format;
     }
 
@@ -47,34 +75,11 @@ class DateTime
     }
 
     /**
-     * @param int $format
-     *
-     * @return self
-     */
-    public function setFormat($format)
-    {
-        $this->format = $format;
-        return $this;
-    }
-
-    /**
      * @return string
      */
     public function getTime()
     {
         return $this->time;
     }
-
-    /**
-     * @param string $time
-     *
-     * @return self
-     */
-    public function setTime($time)
-    {
-        $this->time = $time;
-        return $this;
-    }
-
 
 }
