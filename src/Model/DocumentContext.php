969a82f80dd17b6adef8f2973c6fadecbbbed7f8
Patrick Romowicz <patrick@romowicz.de>
2020-01-30 10:28:20 +0100

    Add TestIndicator to DocumentContext. Thanks #11.

--- src/Model/DocumentContext.php
+++ src/Model/DocumentContext.php
@@ -7,6 +7,14 @@ use JMS\Serializer\Annotation\XmlElement;
 class DocumentContext
 {
     /**
+     * @var \Easybill\ZUGFeRD\Model\Indicator,
+     * @Type("Easybill\ZUGFeRD\Model\Indicator")
+     * @XmlElement(cdata=false, namespace="urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
+     * @SerializedName("TestIndicator")
+     */
+    private $testIndicator;
+
+    /**
      * @var \Easybill\ZUGFeRD\Model\ContextParameterID
      * @Type("Easybill\ZUGFeRD\Model\ContextParameterID")
      * @XmlElement(namespace="urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:12")
@@ -14,8 +22,9 @@ class DocumentContext
      */
     private $type;
 
-    public function __construct($type)
+    public function __construct($type, bool $testIndicator = false)
     {
         $this->type = new ContextParameterID('urn:ferd:CrossIndustryDocument:invoice:1p0:' . strtolower($type));
+        $this->testIndicator = new Indicator($testIndicator);
     }
 }
