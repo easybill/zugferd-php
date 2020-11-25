05cf0852428d9dce78715a4f4ce4ebd43601a819
Patrick Romowicz <romowicz@easybill.de>
2020-11-25 11:04:01 +0100

    Add lineThree to TradeAddress (#29)
    And rename Address to TradeAddress.
    Add github actions.

--- src/zugferd211/Model/Address.php
+++ src/zugferd211/Model/Address.php
@@ -2,44 +2,10 @@
 
 namespace Easybill\ZUGFeRD211\Model;
 
-use JMS\Serializer\Annotation\SerializedName;
-use JMS\Serializer\Annotation\Type;
-use JMS\Serializer\Annotation\XmlElement;
-
-class Address
+/**
+ * @deprecated
+ * @see TradeAddress
+ */
+class Address extends TradeAddress
 {
-    /**
-     * @Type("string")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("PostcodeCode")
-     */
-    public ?string $postcode = null;
-
-    /**
-     * @Type("string")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("LineOne")
-     */
-    public ?string $lineOne = null;
-
-    /**
-     * @Type("string")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("LineTwo")
-     */
-    public ?string $lineTwo = null;
-
-    /**
-     * @Type("string")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("CityName")
-     */
-    public ?string $city = null;
-
-    /**
-     * @Type("string")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("CountryID")
-     */
-    public ?string $countryCode = null;
 }
