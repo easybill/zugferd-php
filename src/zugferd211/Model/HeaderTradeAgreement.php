5a3d0e6e7cdd3011c96b4ac2484ec71e712baa0f
lorenz <schaefer@easybill.de>
2020-12-15 10:19:52 +0100

    allow various attachments

--- src/zugferd211/Model/HeaderTradeAgreement.php
+++ src/zugferd211/Model/HeaderTradeAgreement.php
@@ -5,6 +5,7 @@ namespace Easybill\ZUGFeRD211\Model;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\XmlElement;
+use JMS\Serializer\Annotation\XmlList;
 
 class HeaderTradeAgreement
 {
@@ -37,9 +38,9 @@ class HeaderTradeAgreement
     public ?ReferencedDocument $buyerOrderReferencedDocument = null;
 
     /**
-     * @Type("Easybill\ZUGFeRD211\Model\ReferencedDocument")
-     * @XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @SerializedName("AdditionalReferencedDocument")
+     * @var ReferencedDocument[]
+     * @Type("array<Easybill\ZUGFeRD211\Model\ReferencedDocument>")
+     * @XmlList(inline = true, entry = "AdditionalReferencedDocument", namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
      */
-    public ?ReferencedDocument $additionalReferencedDocument = null;
+    public array $additionalReferencedDocuments = [];
 }
