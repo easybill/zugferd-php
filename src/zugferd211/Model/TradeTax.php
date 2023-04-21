fe36925f14c0d159255b3593fa8257536f6b9610
Pablo Schläpfer <github@spam.pableu.net>
2023-04-21 18:05:39 +0200

    fix a typo in the annotation

--- src/zugferd211/Model/TradeTax.php
+++ src/zugferd211/Model/TradeTax.php
@@ -30,7 +30,7 @@ class TradeTax
     /**
      * @JMS\Type("string")
      * @JMS\XmlElement(cdata = false, namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")
-     * @JMS\SerializedName("ExemptionReasonCode
+     * @JMS\SerializedName("ExemptionReasonCode")
      */
     public ?string $exemptionReasonCode = null;
     
