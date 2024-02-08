1ba7f73497f2f5b77ad2b83da0b52bd1b67ffd80
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-02-08 18:17:34 +0100

    add .php-cs-fixer to handled files

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,19 @@
 <?php
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
-$finder = (new PhpCsFixer\Finder())
-    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+$finder = (new Finder())
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->append([
+        __FILE__,
+    ])
+;
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRiskyAllowed(false)
     ->setRules([
         '@PSR2' => true,
@@ -16,20 +25,21 @@ return (new PhpCsFixer\Config())
         'yoda_style' => false,
         'ordered_class_elements' => false,
         'ordered_imports' => false,
-        //'method_argument_space' => null,
-        //'no_whitespace_in_blank_line' => null,
-        //'no_extra_blank_lines' => null,
-        //'braces' => null,
+        // 'method_argument_space' => null,
+        // 'no_whitespace_in_blank_line' => null,
+        // 'no_extra_blank_lines' => null,
+        // 'braces' => null,
         'blank_line_before_statement' => false,
         'phpdoc_align' => ['align' => 'left'],
         'phpdoc_var_without_name' => false,
         'phpdoc_types_order' => false,
         'phpdoc_order' => false,
         'phpdoc_separation' => false,
-        //'no_superfluous_elseif' => null,
+        // 'no_superfluous_elseif' => null,
         'class_definition' => false,
         'ternary_to_null_coalescing' => true,
         'php_unit_test_class_requires_covers' => false,
         'php_unit_internal_class' => false,
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+;
