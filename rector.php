242e71065d318b1e9b5054a06a8cef6b41b8468d
Jan Nöhles <jan.noehles@easybill.de>
2024-02-29 19:37:42 +0100

    Add declare strict types Drop deprecated Address object

--- rector.php
+++ rector.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Set\JMSSetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
     ])
     ->withPhpSets(php81: true)
+    ->withRules([
+        ReturnTypeFromStrictNativeCallRector::class,
+        ReturnTypeFromStrictScalarReturnExprRector::class,
+    ])
     ->withSets([
         JMSSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
