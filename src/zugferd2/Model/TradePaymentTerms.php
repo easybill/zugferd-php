42d70e398ed090430e7382868a6088fc380eb297
torstenkoehler <t.koehler@bormann-elektrobau.de>
2024-11-11 11:10:59 +0100

    Update TradePaymentTerms.php
    change order DueDateDateTime and ApplicableTradePaymentDiscountTerms

--- src/zugferd2/Model/TradePaymentTerms.php
+++ src/zugferd2/Model/TradePaymentTerms.php
@@ -13,15 +13,15 @@ class TradePaymentTerms
     #[JMS\SerializedName('Description')]
     public ?string $description = null;
 
-    #[JMS\Type(TradePaymentDiscountTerms::class)]
-    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[JMS\SerializedName('ApplicableTradePaymentDiscountTerms')]
-    public ?TradePaymentDiscountTerms $applicableTradePaymentDiscountTerms = null;
-
     #[JMS\Type(DateTime::class)]
     #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     #[JMS\SerializedName('DueDateDateTime')]
     public ?DateTime $dueDate = null;
+
+    #[JMS\Type(TradePaymentDiscountTerms::class)]
+    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[JMS\SerializedName('ApplicableTradePaymentDiscountTerms')]
+    public ?TradePaymentDiscountTerms $applicableTradePaymentDiscountTerms = null;
 
     #[JMS\Type('string')]
     #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
