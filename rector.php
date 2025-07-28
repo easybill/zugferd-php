12e6d30da471e88ee9552ddd62bd85bc0e13ca70
Jan Nöhles <jan.noehles@easybill.de>
2025-07-28 13:35:35 +0200

    Cleanup .php-cs-fixer.dist.php and rector.php

--- rector.php
+++ rector.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Set\JMSSetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src/zugferd2',
     ])
-    ->withPhpSets(php81: true)
-    ->withRules([
-        ReturnTypeFromStrictNativeCallRector::class,
-        ReturnTypeFromStrictScalarReturnExprRector::class,
-    ])
-    ->withSets([
-        JMSSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
+    ->withPhpSets(php82: true)
+    ->withRules([])
+    ->withSets([]);
