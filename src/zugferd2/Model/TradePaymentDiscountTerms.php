7a4ad74505fe70f03832332793c8b5612f724fde
Jan Nöhles <jan.noehles@easybill.de>
2025-09-13 16:31:41 +0200

    Add fields BasisDateTime, BasisAmount and ActualDiscountAmount to TradePaymentDiscountTerms

--- src/zugferd2/Model/TradePaymentDiscountTerms.php
+++ src/zugferd2/Model/TradePaymentDiscountTerms.php
@@ -5,16 +5,34 @@ declare(strict_types=1);
 namespace Easybill\ZUGFeRD2\Model;
 
 use JMS\Serializer\Annotation as JMS;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlElement;
 
 class TradePaymentDiscountTerms
 {
+    #[Type(DateTime::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('BasisDateTime')]
+    public ?DateTime $basisDateTime = null;
+
     #[JMS\Type(Quantity::class)]
     #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     #[JMS\SerializedName('BasisPeriodMeasure')]
     public ?Quantity $basisPeriodMeasure = null;
 
+    #[JMS\Type(Amount::class)]
+    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[JMS\SerializedName('BasisAmount')]
+    public ?Amount $basisAmount = null;
+
     #[JMS\Type('string')]
     #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     #[JMS\SerializedName('CalculationPercent')]
     public ?string $calculationPercent = null;
+
+    #[JMS\Type(Amount::class)]
+    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[JMS\SerializedName('ActualDiscountAmount')]
+    public ?Amount $actualDiscountAmount = null;
 }
