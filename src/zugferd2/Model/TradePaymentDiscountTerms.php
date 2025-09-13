c01068ab26ad3f0aabc57bf9c86dd03a58b6cd04
Jan Nöhles <jan.noehles@easybill.de>
2025-09-13 16:33:26 +0200

    Code Style fix

--- src/zugferd2/Model/TradePaymentDiscountTerms.php
+++ src/zugferd2/Model/TradePaymentDiscountTerms.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Easybill\ZUGFeRD2\Model;
 
-use JMS\Serializer\Annotation as JMS;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\XmlElement;
@@ -16,23 +15,23 @@ class TradePaymentDiscountTerms
     #[SerializedName('BasisDateTime')]
     public ?DateTime $basisDateTime = null;
 
-    #[JMS\Type(Quantity::class)]
-    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[JMS\SerializedName('BasisPeriodMeasure')]
+    #[Type(Quantity::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('BasisPeriodMeasure')]
     public ?Quantity $basisPeriodMeasure = null;
 
-    #[JMS\Type(Amount::class)]
-    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[JMS\SerializedName('BasisAmount')]
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('BasisAmount')]
     public ?Amount $basisAmount = null;
 
-    #[JMS\Type('string')]
-    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[JMS\SerializedName('CalculationPercent')]
+    #[Type('string')]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('CalculationPercent')]
     public ?string $calculationPercent = null;
 
-    #[JMS\Type(Amount::class)]
-    #[JMS\XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[JMS\SerializedName('ActualDiscountAmount')]
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('ActualDiscountAmount')]
     public ?Amount $actualDiscountAmount = null;
 }
