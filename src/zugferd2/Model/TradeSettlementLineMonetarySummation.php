46b0cc94775db5c824cb2cbf6c581c4a62d7f07e
Jan Nöhles <jan.noehles@easybill.de>
2025-09-13 17:18:20 +0200

    Rename totalAmount of TradeSettlementLineMonetarySummation ti lineTotalAmount Add missing fields to TradeSettlementLineMonetarySummation and add to the fields to the constructor method Remove gitter from readme

--- src/zugferd2/Model/TradeSettlementLineMonetarySummation.php
+++ src/zugferd2/Model/TradeSettlementLineMonetarySummation.php
@@ -13,18 +13,59 @@ class TradeSettlementLineMonetarySummation
     #[Type(Amount::class)]
     #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     #[SerializedName('LineTotalAmount')]
-    public Amount $totalAmount;
+    public Amount $lineTotalAmount;
+
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('ChargeTotalAmount')]
+    public ?Amount $chargeTotalAmount = null;
+
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('AllowanceTotalAmount')]
+    public ?Amount $allowanceTotalAmount = null;
+
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('TaxTotalAmount')]
+    public ?Amount $taxTotalAmount = null;
+
+    #[Type(Amount::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('GrandTotalAmount')]
+    public ?Amount $grandTotalAmount = null;
 
     #[Type(Amount::class)]
     #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     #[SerializedName('TotalAllowanceChargeAmount')]
     public ?Amount $totalAllowanceChargeAmount = null;
 
-    public static function create(string $totalAmount, ?string $totalAllowanceChargeAmount = null): self
-    {
+    public static function create(
+        string $totalAmount,
+        ?string $chargeTotalAmount = null,
+        ?string $allowanceTotalAmount = null,
+        ?string $taxTotalAmount = null,
+        ?string $grandTotalAmount = null,
+        ?string $totalAllowanceChargeAmount = null
+    ): self {
         $self = new self();
+        $self->lineTotalAmount = Amount::create($totalAmount);
 
-        $self->totalAmount = Amount::create($totalAmount);
+        if ($chargeTotalAmount !== null) {
+            $self->chargeTotalAmount = Amount::create($chargeTotalAmount);
+        }
+
+        if ($allowanceTotalAmount !== null) {
+            $self->allowanceTotalAmount = Amount::create($allowanceTotalAmount);
+        }
+
+        if ($taxTotalAmount !== null) {
+            $self->taxTotalAmount = Amount::create($taxTotalAmount);
+        }
+
+        if ($grandTotalAmount !== null) {
+            $self->grandTotalAmount = Amount::create($grandTotalAmount);
+        }
 
         if ($totalAllowanceChargeAmount !== null) {
             $self->totalAllowanceChargeAmount = Amount::create($totalAllowanceChargeAmount);
