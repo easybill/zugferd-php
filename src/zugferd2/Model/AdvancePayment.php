3a97cfbaa6acc70243b5ae3bfad9d346113a080c
Jan Nöhles <jan.noehles@easybill.de>
2025-12-08 20:59:19 +0100

    Remove ReceivedDateTime from AdvancePayment
    Add InvoiceSpecifiedReferencedDocument to AdvancePayment

--- src/zugferd2/Model/AdvancePayment.php
+++ src/zugferd2/Model/AdvancePayment.php
@@ -21,13 +21,13 @@ class AdvancePayment
     #[SerializedName('FormattedReceivedDateTime')]
     public ?FormattedDateTime $formattedReceivedDateTime = null;
 
-    #[Type(DateTime::class)]
-    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
-    #[SerializedName('ReceivedDateTime')]
-    public ?DateTime $receivedDateTime = null;
-
     /** @var TradeTax[] */
     #[Type('array<Easybill\ZUGFeRD2\Model\TradeTax>')]
     #[XmlList(entry: 'IncludedTradeTax', inline: true, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
     public array $includedTradeTax = [];
+
+    #[Type(ReferencedDocument::class)]
+    #[XmlElement(cdata: false, namespace: 'urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100')]
+    #[SerializedName('InvoiceSpecifiedReferencedDocument')]
+    public ?ReferencedDocument $invoiceSpecifiedReferencedDocument = null;
 }
