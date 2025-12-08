1855119e4a625ae6a974e7116202cd8cac353f17
Jan Nöhles <jan.noehles@easybill.de>
2025-12-08 20:56:31 +0100

    Sort xmlns attributes in tests

--- tests/zugferd2/Tests/Traits/ReformatXmlTrait.php
+++ tests/zugferd2/Tests/Traits/ReformatXmlTrait.php
@@ -14,6 +14,50 @@ trait ReformatXmlTrait
         $doc->preserveWhiteSpace = false;
         $doc->formatOutput = true;
         $doc->loadXML($xml);
-        return (string)$doc->saveXML();
+        $result = (string)$doc->saveXML();
+
+        return self::sortRootXmlnsAttributes($result);
+    }
+
+    // In the newer examples (2.4) the root xmlns attributes differ from the previous version
+    // to avoid any conflicts we just sort them.
+    private static function sortRootXmlnsAttributes(string $xml): string
+    {
+        if (!preg_match('/^(<\?xml[^?]*\?>\s*)?(<[a-zA-Z0-9:]+)\s+([^>]+)>/s', $xml, $matches)) {
+            return $xml;
+        }
+
+        $xmlDecl = $matches[1];
+        $rootTag = $matches[2];
+        $attributesStr = $matches[3];
+
+        preg_match_all('/([a-zA-Z0-9:_-]+)="([^"]*)"/', $attributesStr, $attrMatches, PREG_SET_ORDER);
+
+        $attributes = [];
+        foreach ($attrMatches as $attr) {
+            $name = $attr[1];
+            $value = $attr[2];
+
+            if (str_starts_with($name, 'xmlns:')) {
+                $prefix = substr($name, 6);
+
+                if (!preg_match('/<' . preg_quote($prefix, '/') . ':/', $xml)) {
+                    continue;
+                }
+            }
+
+            $attributes[$name] = $value;
+        }
+
+        ksort($attributes);
+
+        $newAttributes = [];
+        foreach ($attributes as $name => $value) {
+            $newAttributes[] = $name . '="' . $value . '"';
+        }
+
+        $newRootElement = $rootTag . ' ' . implode(' ', $newAttributes) . '>';
+
+        return preg_replace('/^(<\?xml[^?]*\?>\s*)?<[a-zA-Z0-9:]+\s+[^>]+>/s', $xmlDecl . $newRootElement, $xml, 1);
     }
 }
